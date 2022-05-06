a699896d3ea4941835519af6bc72f8c138648636
nad3in <nadinelsaeed@hotmail.com>
2022-05-06 11:34:05 +0200

    upated cors url

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cors = require('cors')
 app.use(cors())
 app.use(express.json());
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://covid-tracker-na.herokuapp.com/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token');
 
